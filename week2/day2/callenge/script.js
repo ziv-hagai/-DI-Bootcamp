5ab0ac5a7aac68a0bca2f5b94931ea70362a7054
ziv-hagai <zivhag@gmail.com>
2022-03-22 09:57:49 +0200

    relevant comment

--- week2/day2/callenge/script.js
+++ week2/day2/callenge/script.js
@@ -2,24 +2,23 @@ let sentence = 'The movie is not that bad, I like it';
 let wordNot = sentence.indexOf('not');
 let wordBad = sentence.indexOf('bad');
 
-if (wordNot<wordBad) {
-	notBad = sentence.slice(wordNot,wordBad+3);
+if (wordNot<wordBad && wordBad >= 0 && wordNot >= 0) {
+	notBad = sentence.slice(wordNot,wordBad+3) ;
 	let newSentence = sentence.replace(notBad, "good");
 	console.log(newSentence)
-}
-
-else {
+} else {
 	console.log(sentence)
 }
-let str = "Happy BirthDay";
-let patt = /birthday/i;
-let result = str.match(patt);
-console.log(result); //returns true
 
-if (result){
-	console.log('Yes')
-} else{
-	console.log('No');
-}
-let regex = /^.+@.+\..+$/;
-console.log(regex.test('johndoe@gmail.com')); //returns true
+// let str = "Happy BirthDay";
+// let patt = /birthday/i;
+// let result = str.match(patt);
+// console.log(result); //returns true
+
+// if (result){
+// 	console.log('Yes')
+// } else{
+// 	console.log('No');
+// }
+// let regex = /^.+@.+\..+$/;
+// console.log(regex.test('johndoe@gmail.com')); //returns true
