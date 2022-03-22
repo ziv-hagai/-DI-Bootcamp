7372e5312ac266ce4a6685669c74e39925f566d8
ziv-hagai <zivhag@gmail.com>
2022-03-22 17:34:49 +0200

    relevant comment

--- week2/day3/callenge/script.js
+++ week2/day3/callenge/script.js
@@ -1,24 +1,17 @@
-let sentence = 'The movie is not that bad, I like it';
-let wordNot = sentence.indexOf('not');
-let wordBad = sentence.indexOf('bad');
-
-if (wordNot<wordBad && wordBad >= 0 && wordNot >= 0) {
-	notBad = sentence.slice(wordNot,wordBad+3);
-	let newSentence = sentence.replace(notBad, "good");
-	console.log(newSentence)
-} else {
-	console.log(sentence)
+for (let i = 0; i < 7; i++) {
+	let asterisk = '* ';
+	let pyramid = asterisk.repeat(i) + "\n"
+	console.log(pyramid);
 }
 
-// let str = "Happy BirthDay";
-// let patt = /birthday/i;
-// let result = str.match(patt);
-// console.log(result); //returns true
 
-// if (result){
-// 	console.log('Yes')
-// } else{
-// 	console.log('No');
-// }
-// let regex = /^.+@.+\..+$/;
-// console.log(regex.test('johndoe@gmail.com')); //returns true
+// nested
+
+for (let i = 1; i < 7; i++) {
+	let pyramid = "";
+	for (let j = 1; j <= i; j++) {
+		pyramid += "* ";
+
+	}
+	console.log(pyramid);
+}
