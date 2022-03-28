d06a853552c1c3dc915e19da6b44ce81462c932d
ziv-hagai <zivhag@gmail.com>
2022-03-28 10:56:22 +0300

    relevant comment

--- week3/day1/challenge/script.js
+++ week3/day1/challenge/script.js
@@ -1,13 +1,31 @@
 
-// let planet = document.getElementsByTagName('section').createElement('div').style.background = 'yellow' 	
-const planets = ["blue", "red", "orange", "green", "yellow", "lightgreen", "gray", "lightblue"]
+const planets = [
+{name:"mercury", color:"orange", moons:0},
+{name:"venus", color:"pink", moons:0},
+{name:"earth", color:"lightblue", moons:1},
+{name:"mars", color:"red", moons:2},
+{name:"jupiter", color:"gray", moons:53},
+{name:"saturn", color:"yellow", moons:53},
+{name:"uranus", color:"blue", moons:27},
+{name:"mercury", color:"green", moons:14}
+]
 
-for (let i=0; i<planets.length; i++) {
+function solar () {
+	
+	for (let i=0; i<planets.length; i++) {
 
-	let newPlanet = document.createElement("div");
-	newPlanet.classList.add("planet");
-	newPlanet.style.background = planets[i];
-// newPlanet.innerHTML = "newPlanet";
-document.querySelector(".listPlanets").appendChild(newPlanet); 
+		let newPlanet = document.createElement("div");
+		newPlanet.classList.add("planet");
+		newPlanet.style.background = planets[i].color;
+		newPlanet.innerHTML = planets[i].name;
+		document.querySelector(".listPlanets").appendChild(newPlanet); 
+
+		for (let j=0; j<planets[i].moons; j++) {
+			let newMoon = document.createElement("div");
+			newMoon.classList.add("moon");
+			document.querySelector(".listPlanets").lastElementChild.appendChild(newMoon); 
+		}
+	}
 }
-pur
+
+solar()
