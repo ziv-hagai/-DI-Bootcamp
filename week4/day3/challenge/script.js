88a2ad4221f9cc37559a9499ea9424cb1b6213cc
ziv-hagai <zivhag@gmail.com>
2022-04-05 17:22:48 +0300

    relevant comment

--- week4/day3/challenge/script.js
+++ week4/day3/challenge/script.js
@@ -1,32 +1,22 @@
-let client = "John";
+let inventory = [
+{ id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+{ id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+{ id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+{ id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+{ id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+];
 
-let groceries = {
-    fruits : ["pear", "apple", "banana"],
-    vegetables: ["tomatoes", "cucumber", "salad"],
-    totalPrice : "20$",
-    other : {
-        payed : true,
-        meansOfPayment : ["cash", "creditCard"]
-    }
+function getCarHonda(carInventory) {
+    let firstHonda = inventory.find(item => item.car_make === "Honda");
+    let thisCar = `This is a ${firstHonda.car_make} ${firstHonda.car_model} from ${firstHonda.car_year}`
+    return thisCar;
 }
 
-
-const displayGroceries = () => {
-    groceries.fruits.forEach( function(element) {
-        console.log(element)
-    });
+function sortCarInventoryByYear(carInventory) {
+    let sorted = [...inventory];
+    sorted = sorted.sort(function (x,y) {
+        return x.car_year - y.car_year;
+    })
 }
 
-const cloneGroceries = () => {
-    let user = client;
-    client = "Betty";
-    // user stays "john" becuase client is global and user local
-    let shopping = groceries;
-    groceries.totalPrice = "35$";
-    //same 
-    groceries.other.payed = "false";
-    console.log(shopping)
-    //not sure
-}
-
-cloneGroceries()
+sortCarInventoryByYear(inventory)
