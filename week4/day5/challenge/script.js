2ed1750fb1b6384540977a95ec54303501be8fcb
ziv-hagai <zivhag@gmail.com>
2022-04-08 11:25:49 +0300

    relevant comment

--- week4/day5/challenge/script.js
+++ week4/day5/challenge/script.js
@@ -1,18 +1,5 @@
-class Video {
-    constructor(title, uploader, time) {
-        this.title = title;
-        this.uploader = uploader;
-        this.time = time;
-    }
-    watch() {
-        console.log(`${this.uploader} watched all ${this.time} of ${this.title}`);
-        return this;
-    }
+function isAnagram (a,b) {
+    let first = a.toLowerCase().split('').sort().join('').trim();
+    let second = b.toLowerCase().split('').sort().join('').trim();
+    return (first === second);
 }
-
-let clip = new Video("batman", 'John', 11);
-clip.watch();
-
-
-let clipTwo = new Video("superman", 'Jo', 12);
-clipTwo.watch();
