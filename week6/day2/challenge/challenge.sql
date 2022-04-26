223407727073959c67d76c1e58900fa9aa1d227b
ziv-hagai <zivhag@gmail.com>
2022-04-26 09:21:46 +0300

    relevant comment

--- week6/day2/challenge/challenge.sql
+++ week6/day2/challenge/challenge.sql
@@ -19,15 +19,26 @@ INSERT INTO SecondTab VALUES
 (5),
 (NULL)
 
-
 SELECT * FROM SecondTab
 
 -- 1 
 SELECT COUNT(*) 
     FROM FirstTab AS ft 
 	WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL )
--- ID  Name
--- 5   Pawan
--- 6   Sharlee
--- 7   Krish
+-- count: 0
+
+-- 2
+ SELECT COUNT(*) 
+    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
+-- count: 2
+
+-- 	3
+SELECT COUNT(*) 
+    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
+-- count: 3
+
+-- 	4
+SELECT COUNT(*) 
+    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
+-- count: 3
 
