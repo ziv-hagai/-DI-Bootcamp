058320672d37253cc55057a871996a8338953cc1
ziv-hagai <zivhag@gmail.com>
2022-05-10 21:23:16 +0300

    relevant comment

--- week7/day4/public/script.js
+++ week7/day4/public/script.js
@@ -1,8 +1,18 @@
-function add() {
-    const name = document.getElementById('name').value;
-    const price = document.getElementById('price').value;
-    console.log(name, price);
-    fetch('http://localhost:5002/api/product', {
+let fname = document.getElementById('fname').value;
+let lname = document.getElementById('lname').value;
+let email = document.getElementById('email').value;
+let username = document.getElementById('username').value;
+let button = document.getElementById('button');
+button.removeAttribute('disabled'); 
+console.log(fname);
+
+if (fname && lname && email && username){
+button.removeAttribute('disabled'); 
+}
+
+function register() {
+    console.log(fname, lname, email, username);
+    fetch('http://localhost:5002/register', {
         method: 'POST',
         headers: {
             'Content-type': 'application/json'
@@ -18,36 +28,36 @@ function add() {
     })
 }
 
-function deletep() {
-    const id = document.getElementById('id').value;
-    fetch(`http://localhost:5002/api/product/${id}`, {
-        method: 'DELETE'
-})
-.then(res => res.json())
-    .then(product=>{
-        console.log(product);
-    })
-    .catch(err=>{
-        console.log(err)
-})
-}
+// function deletep() {
+//     const id = document.getElementById('id').value;
+//     fetch(`http://localhost:5002/api/product/${id}`, {
+//         method: 'DELETE'
+// })
+// .then(res => res.json())
+//     .then(product=>{
+//         console.log(product);
+//     })
+//     .catch(err=>{
+//         console.log(err)
+// })
+// }
 
-function updatep() {
-    const name = document.getElementById('namep').value;
-    const price = document.getElementById('pricep').value;
-    const id = document.getElementById('idp').value;
-    fetch(`http://localhost:5002/api/product/${id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body:JSON.stringify({name,price})
-    })
-.then(res => res.json())
-    .then(product=>{
-        console.log(product);
-    })
-    .catch(err=>{
-        console.log(err)
-})
-}
+// function updatep() {
+//     const name = document.getElementById('namep').value;
+//     const price = document.getElementById('pricep').value;
+//     const id = document.getElementById('idp').value;
+//     fetch(`http://localhost:5002/api/product/${id}`, {
+//         method: 'PUT',
+//         headers: {
+//             'Content-type': 'application/json'
+//         },
+//         body:JSON.stringify({name,price})
+//     })
+// .then(res => res.json())
+//     .then(product=>{
+//         console.log(product);
+//     })
+//     .catch(err=>{
+//         console.log(err)
+// })
+// }
