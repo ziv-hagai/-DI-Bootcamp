04aefac33edec1ca5b90fbd18843dbf2b91d405b
ziv-hagai <zivhag@gmail.com>
2022-06-08 14:58:37 +0300

    relevant comment

--- arithmax/server.js
+++ arithmax/server.js
@@ -2,62 +2,70 @@ const express = require('express');
 const dotenv = require('dotenv');
 const knex = require('knex');
 const app = express();
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
+const path = require('path');
+const cors = require('cors');
 
 dotenv.config();
 
-app.listen(process.env.PORT||8080, ()=>{
-  console.log(`listen on port ${process.env.PORT||8080}`);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`listen on port ${process.env.PORT || 8080}`);
 })
 
 const db = knex({
-  client:'pg',
-  connection:{
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   }
 });
 
-app.use('/', express.static(__dirname+'/public'));
-
-app.get('/records',(req,res) => {
-  res.sendFile(__dirname+'/public/records.html')
+app.get('/records', (req, res) => {
+  res.sendFile(__dirname + '/public/records.html')
 })
 
-app.get('/play',(req,res) => {
-  res.sendFile(__dirname+'/public/play.html')
+app.get('/play', (req, res) => {
+  res.sendFile(__dirname + '/public/play.html')
 })
 
 //display records
-app.post('/records',(req,res) => {
-return db('records')
-    .select('player_name','record', 'record_date')
+app.post('/records', (req, res) => {
+  return db('records')
+    .select('player_name', 'record', 'record_date')
     .orderBy('record', 'desc')
     .limit(5)
     .returning('*')
-    .then(records=>{
+    .then(records => {
       res.json(records)
     })
-  .catch(err => {
-  console.log(err);
-  res.json({message:err.message});
-  })
+    .catch(err => {
+      console.log(err);
+      res.json({ message: err.message });
+    })
 })
 
 //add a record
-app.post('/db', (req,res) => {
+app.post('/db', (req, res) => {
   return db('records')
-.insert(req.body)
-.returning('*')
-.then(data => {
-  console.log(data);
-  res.json(data)
+    .insert(req.body)
+    .returning('*')
+    .then(data => {
+      console.log(data);
+      res.json(data)
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
-.catch(err => {
-  console.log(err);
-})
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname + './public', 'index.html'))
 })
